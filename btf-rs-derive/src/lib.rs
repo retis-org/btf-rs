f3dce3dd7467dc9db6c3bb83d598af99c501f7c7
Antoine Tenart <atenart@redhat.com>
2025-08-08 11:30:34 +0200

    cbtf: derive the from_reader helper
    Instead of repeating the same logic all over the place for constructing a cbtf
    type from a reader, derive it. This improves maintenance and will help adding
    future common helpers to cbtf types.
    
    A new crate, btf-rs-derive, is added to implement the derive logic.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

---
+++ btf-rs-derive/src/lib.rs
@@ -0,0 +1,57 @@
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::*;
+
+#[proc_macro_derive(CBtfType)]
+pub fn cbtf_type_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+
+    let fields = match ast.data {
+        Data::Struct(data) => match data.fields {
+            Fields::Named(fields) => fields.named,
+            _ => panic!("{name} is not a struct with named fields"),
+        },
+        _ => panic!("{name} is not a struct"),
+    };
+
+    let reader_fields = fields
+        .iter()
+        .map(|f| gen_reader_field(f.ident.as_ref().unwrap(), &f.ty));
+
+    quote! {
+        impl #name {
+            pub(super) fn from_reader<R: std::io::Read>(
+                reader: &mut R,
+                endianness: &crate::cbtf::Endianness,
+            ) -> crate::Result<Self> {
+                Ok(#name {
+                    #( #reader_fields )*
+                })
+            }
+        }
+    }
+    .into()
+}
+
+// Generate struct fields initialization using the input data from a reader.
+// e.g. `u32 val: endianness.u32_from_reader(reader)?,`
+fn gen_reader_field(ident: &Ident, r#type: &Type) -> proc_macro2::TokenStream {
+    let ty = match r#type {
+        Type::Path(tp) => &tp.path,
+        _ => panic!("Field {ident:?} is not a plain type"),
+    };
+
+    match ty.to_token_stream().to_string().as_str() {
+        "u16" => quote! {
+            #ident: endianness.u16_from_reader(reader)?,
+        },
+        "u32" => quote! {
+            #ident: endianness.u32_from_reader(reader)?,
+        },
+        "i32" => quote! {
+            #ident: endianness.i32_from_reader(reader)?,
+        },
+        ty => panic!("Unsupported field type ({ty})"),
+    }
+}
