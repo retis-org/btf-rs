fd6d2eba3ce1935cdab6e0e70c97621a224e93c9
Antoine Tenart <atenart@redhat.com>
2026-04-15 17:54:46 +0200

    tests: generate kind ids
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

---
+++ btf-rs/tests/assets/ids.rs
@@ -0,0 +1,6 @@
+pub(super) const BTF_ID_INT: u32 = 21;
+pub(super) const BTF_ID_U64: u32 = 36;
+pub(super) const BTF_ID_SK_BUFF: u32 = 1768;
+pub(super) const BTF_ID_KFREE_SKB_REASON: u32 = 26255;
+pub(super) const BTF_ID_OVS_DROP_REASON: u32 = 36007;
+pub(super) const BTF_ID_CONST: u32 = 4;
